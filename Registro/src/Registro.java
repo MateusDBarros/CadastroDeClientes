cf1dc756a68b0af90a0fbea5c4d181881ed0bd33
Mateus de Barros <97922225+MateusDBarros@users.noreply.github.com>
2024-11-08 13:32:59 -0300

    Update Registro.java

--- Registro/src/Registro.java
+++ Registro/src/Registro.java
@@ -16,10 +16,25 @@ public class Registro {
             System.out.printf("Digite a idade de " + dados[numPessoas].name + ": ");
             dados[numPessoas].age = scanner.nextInt();
             dados[numPessoas].id = numPessoas + 1;
-            numPessoas++;
+            Registro.numPessoas++;
             System.out.println("Cadastro Registrado com sucesso!");
         }
         else
             System.out.println("Impossivel adicionar mais registros!");
+    }
+
+    public static void exibir(Registro dados[], int numPessoas) {
+        if (numPessoas == 0) {
+            System.out.println("Nenhum cadastro registrado!");
+            return;
+        }
+
+        for (int i = 0; i < numPessoas; i++) {
+            System.out.println("ID:\t" + dados[i].id);
+            System.out.println("Nome:\t" + dados[i].name);
+            System.out.println("Idade:\t" + dados[i].age);
+            System.out.println();
+
+        }
     }
 }
