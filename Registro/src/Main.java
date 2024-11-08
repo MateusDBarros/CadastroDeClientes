1c1d161cddfe64e3b30f8454dc3c7ee6937303b6
Mateus de Barros <97922225+MateusDBarros@users.noreply.github.com>
2024-11-08 13:32:41 -0300

    Update Main.java

--- Registro/src/Main.java
+++ Registro/src/Main.java
@@ -1,14 +1,43 @@
+import  java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         Registro dados[] = new Registro[100];
 
-        int pessoas = Registro.numPessoas;
-        Registro.adicionar(dados, pessoas);
+        int escolha;
+        do {
+            System.out.println("1. Criar Cadastro.");
+            System.out.println("2. Exibir Cadastros.");
+            System.out.println("3. Pesquisar Cadastro.");
+            System.out.println("4. Deletar Cadastro.");
+            System.out.println("5. Sair.");
+            escolha = scanner.nextInt();
 
-        for (int i = 0; i < pessoas + 1; i++) {
-            System.out.println("ID: "  + dados[pessoas].id + " Nome: " + dados[pessoas].name + " Idade: " +dados[pessoas].age);
-        }
+            switch (escolha) {
+                case 1:
+                    Registro.adicionar(dados, Registro.numPessoas);
+                    break;
 
+                case 2:
+                    Registro.exibir(dados, Registro.numPessoas);
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                default:
+                    System.out.println("Opção invalida!");
+                    break;
+            }
+
+
+        } while (5 != escolha);
     }
+
 }
